5c534b0d881d27c6bb73806def257d1ef49f38c3
majiale <2281525307@qq.com>
2021-07-20 18:07:47 +0800

    Revert "feat(all): 增加UUID工具类和Time工具类"
    This reverts commit aa08cd6c

--- src/main/java/com/github/pangju666/utils/sys/TimeUtils.java
+++
@@ -1,9 +0,0 @@
-package com.github.pangju666.utils.sys;
-
-/**
- * @author pangju666
- */
-public class TimeUtils {
-    public static final Integer SECOND = 60 * 60;
-    public static final Integer MINUTE = 60 * SECOND;
-}
