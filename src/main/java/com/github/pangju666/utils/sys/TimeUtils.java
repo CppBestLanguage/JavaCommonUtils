aa08cd6c16ec3f8d7105fc750328ea1a71333cae
majiale <2281525307@qq.com>
2021-07-20 18:07:02 +0800

    feat(all): 生成数据表业务代码，整合jwt

---
+++ src/main/java/com/github/pangju666/utils/sys/TimeUtils.java
@@ -0,0 +1,9 @@
+package com.github.pangju666.utils.sys;
+
+/**
+ * @author pangju666
+ */
+public class TimeUtils {
+    public static final Integer SECOND = 60 * 60;
+    public static final Integer MINUTE = 60 * SECOND;
+}
