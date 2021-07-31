646aa8f9485702da5f39b86eeb3ac9870ce1bf89
majiale <2281525307@qq.com>
2021-07-31 17:51:36 +0800

    perf(BeanUtils): 优化转换列表时频繁创建Copier的问题

--- src/main/java/com/github/pangju666/utils/common/BeanUtils.java
+++ src/main/java/com/github/pangju666/utils/common/BeanUtils.java
@@ -12,26 +12,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BeanUtils {
-    public static <T, R> R convert(T entity, Class<R> targetClass) throws RuntimeException {
-        // 获取源对象类型
-        Class<T> sourceClass = getGenericClass(new TypeToken<T>() {});
-        try {
-            R target = targetClass.getDeclaredConstructor().newInstance();
-            BeanCopier copier = getBeanCopier(sourceClass, targetClass);
-            copier.copy(entity, target, null);
-            return target;
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-           throw new RuntimeException("转换失败", e);
-        }
-    }
-
-    public static <T, R> void convert(T source, R target) {
-        if (source != null) {
-            BeanCopier copier = getBeanCopier(new TypeToken<T>() {}, new TypeToken<R>() {});
-            copier.copy(source, target, null);
-        }
-    }
-
     public static <T, R> R convert(T source, Function<T, R> convertFunc) {
         if (source == null) {
             return null;
@@ -39,15 +19,46 @@ public class BeanUtils {
         return convertFunc.apply(source);
     }
 
+    public static <T, R> List<R> convertList(List<T> sourceList, Function<T, R> convertFunc) {
+        return sourceList.stream().map(convertFunc).collect(Collectors.toList());
+    }
+
+    public static <T, R> void convert(T source, R target) {
+        if (source != null && target != null) {
+            BeanCopier copier = getBeanCopier(new TypeToken<T>() {}, new TypeToken<R>() {});
+            convert(source, target, copier);
+        }
+    }
+
+    public static <T, R> R convert(T entity, Class<R> targetClass) throws RuntimeException {
+        // 获取源对象类型
+        Class<T> sourceClass = getGenericClass(new TypeToken<T>() {});
+        BeanCopier copier = getBeanCopier(sourceClass, targetClass);
+        return convert(entity, targetClass, copier);
+    }
+
     public static <T, R> List<R> convertList(List<T> sourceList, Class<R> targetClass) throws RuntimeException {
-        return sourceList.stream().map(source -> convert(source, targetClass)).collect(Collectors.toList());
+        BeanCopier copier = getBeanCopier(new TypeToken<T>() {}, new TypeToken<R>() {});
+        return sourceList.stream()
+                .map(source -> convert(source, targetClass, copier))
+                .collect(Collectors.toList());
     }
 
-    public static <T, R> List<R> convertList(List<T> entityList, Function<T, R> convertFunc) {
-        return entityList.stream().map(convertFunc).collect(Collectors.toList());
+    public static <T, R> R convert(T source, Class<R> targetClass, BeanCopier copier) throws RuntimeException {
+        try {
+            R target = targetClass.getDeclaredConstructor().newInstance();
+            convert(source, target, copier);
+            return target;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException("转换失败", e);
+        }
     }
 
-    private static <T, R> BeanCopier getBeanCopier(TypeToken<T> sourceTypeToken, TypeToken<R> targetTypeToken) {
+    public static <T, R> void convert(T entity, R target, BeanCopier copier) throws RuntimeException {
+        copier.copy(entity, target, null);
+    }
+
+    protected static <T, R> BeanCopier getBeanCopier(TypeToken<T> sourceTypeToken, TypeToken<R> targetTypeToken) {
         // 获取源对象类型
         Class<T> sourceClass = getGenericClass(sourceTypeToken);
         // 获取目标对象类型
@@ -55,12 +66,12 @@ public class BeanUtils {
         return getBeanCopier(sourceClass, targetClass);
     }
 
-    private static <T, R> BeanCopier getBeanCopier(Class<T> sourceClass, Class<R> targetClass) {
+    protected static <T, R> BeanCopier getBeanCopier(Class<T> sourceClass, Class<R> targetClass) {
         return BeanCopier.create(sourceClass, targetClass, false);
     }
 
     @SuppressWarnings("unchecked")
-    private static <T> Class<T> getGenericClass(TypeToken<T> value) {
+    protected static <T> Class<T> getGenericClass(TypeToken<T> value) {
         Type type = value.getClass().getGenericSuperclass();
         ParameterizedType genericType = (ParameterizedType) type;
         return (Class<T>) genericType.getActualTypeArguments()[0];
