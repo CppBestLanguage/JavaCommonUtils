61ceba4be070de1a8bb5416a1d16fc27c8492be6
majiale <2281525307@qq.com>
2021-07-31 17:01:31 +0800

    feat(src): 增加bean工具类，用于类属性和列表之间的转换

---
+++ src/main/java/com/github/pangju666/utils/common/BeanUtils.java
@@ -0,0 +1,68 @@
+package com.github.pangju666.utils.common;
+
+import com.google.gson.reflect.TypeToken;
+
+import net.sf.cglib.beans.BeanCopier;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class BeanUtils {
+    public static <T, R> R convert(T entity, Class<R> targetClass) throws RuntimeException {
+        // 获取源对象类型
+        Class<T> sourceClass = getGenericClass(new TypeToken<T>() {});
+        try {
+            R target = targetClass.getDeclaredConstructor().newInstance();
+            BeanCopier copier = getBeanCopier(sourceClass, targetClass);
+            copier.copy(entity, target, null);
+            return target;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+           throw new RuntimeException("转换失败", e);
+        }
+    }
+
+    public static <T, R> void convert(T source, R target) {
+        if (source != null) {
+            BeanCopier copier = getBeanCopier(new TypeToken<T>() {}, new TypeToken<R>() {});
+            copier.copy(source, target, null);
+        }
+    }
+
+    public static <T, R> R convert(T source, Function<T, R> convertFunc) {
+        if (source == null) {
+            return null;
+        }
+        return convertFunc.apply(source);
+    }
+
+    public static <T, R> List<R> convertList(List<T> sourceList, Class<R> targetClass) throws RuntimeException {
+        return sourceList.stream().map(source -> convert(source, targetClass)).collect(Collectors.toList());
+    }
+
+    public static <T, R> List<R> convertList(List<T> entityList, Function<T, R> convertFunc) {
+        return entityList.stream().map(convertFunc).collect(Collectors.toList());
+    }
+
+    private static <T, R> BeanCopier getBeanCopier(TypeToken<T> sourceTypeToken, TypeToken<R> targetTypeToken) {
+        // 获取源对象类型
+        Class<T> sourceClass = getGenericClass(sourceTypeToken);
+        // 获取目标对象类型
+        Class<R> targetClass = getGenericClass(targetTypeToken);
+        return getBeanCopier(sourceClass, targetClass);
+    }
+
+    private static <T, R> BeanCopier getBeanCopier(Class<T> sourceClass, Class<R> targetClass) {
+        return BeanCopier.create(sourceClass, targetClass, false);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> Class<T> getGenericClass(TypeToken<T> value) {
+        Type type = value.getClass().getGenericSuperclass();
+        ParameterizedType genericType = (ParameterizedType) type;
+        return (Class<T>) genericType.getActualTypeArguments()[0];
+    }
+}
